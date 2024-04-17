8af588a95038bad3ee0b59d4b8fce6fc16da44b5
Jon Niehof <jtniehof@gmail.com>
2024-04-17 11:24:43 -0400

    phep_to_yaml.py: try to extract DOI

--- phep_to_yaml.py
+++ phep_to_yaml.py
@@ -27,6 +27,7 @@ for ph in phep_src:
         else:
             warnings.warn(f"{ph} preamble has no closing code fence, skipping.")
             continue
+        body = f.readlines()
     phep = dict(email.message_from_string("".join(headers)))
     required = (
         "PHEP", "Title", "Author", "Discussions-To", "Revision", "Status",
@@ -45,6 +46,20 @@ for ph in phep_src:
     for k in ('PHEP', 'Revision'):
         phep[k] = int(phep[k])
     phep["Filename"] = os.path.basename(ph)
+    idx = next((i for i, l in enumerate(body)
+                if l.startswith("# Copyright")), None)
+    if idx is not None:
+        doi_lines = [l for l in body[idx:] if " doi " in l]
+        for doi_line in doi_lines:
+            m = re.search(r"10\.\d{4}(\.\d+)*/[-._;()/:A-Z0-9]+",
+                          doi_line, flags=re.IGNORECASE)
+            if m:
+                phep["DOI"] = m.group(0)
+                break
+    if "DOI" not in phep:
+        warnings.warn(
+            f"Found no DOI in {ph}, must fix output by hand.")
+        phep["DOI"] = "Unknown"
     pheps.append(phep)
 with open("pheps.yml", "wt") as f:
     f.write(yaml.dump(pheps))
